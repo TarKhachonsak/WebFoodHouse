baccd8a281696c5d9b4813014bd0ef30613a68b2
TarKhachonsak <khachonsak117@gmail.com>
2024-07-18 14:10:01 +0700

    เกือบเสร็จ

--- src/component/Registration.jsx
+++ src/component/Registration.jsx
@@ -1,19 +1,24 @@
 import logo from '../assets/foodhouse.png'
-
+import { FaStar } from "react-icons/fa";
 
 const Registration = () => {
     return (
         <div className="flex justify-center items-center min-h-screen bg-[#50D6FF]">
             <div className="bg-white rounded-lg shadow-lg overflow-hidden md:flex w-full md:max-w-4xl">
-                <div>
-                    <img src={logo}/>
-                </div>
-                <div className="bg-[#FFF346] py-56 flex flex-col justify-center items-center w-full md:w-1/2 p-6 h-full">
-                    <h2 className="text-2xl font-bold">Welcome!</h2>
-                    <p className="text-center mb-4">Log in to enjoy the new Food house and menu recipes.</p>
+                <div className="bg-[#FFF346] flex flex-col justify-between items-center w-full md:w-1/2 h-full p-6">
+                    <div className='w-1/2 m-20 relative bottom-24 flex justify-center'>
+                        <img src={logo} alt='Food House Logo' />
+                    </div>
+                    <div className='relative bottom-44 flex flex-col justify-center items-center flex-grow text-center'>
+                        <div>
+                            <FaStar className='text-[#FBB9B9] w-96 h-5 relative left-36 bottom-3' />
+                        </div>
+                        <h2 className="text-6xl font-bold text-shadow text-black">Welcome !</h2>
+                        <p className="text-center mb-4 text-sm">Log in to enjoy the new Food house and menu recipes.</p>
+                    </div>
                 </div>
                 <div className="w-full md:w-1/2 px-6 py-10">
-                    <h2 className="text-2xl font-bold text-center mb-4">Register</h2>
+                    <h2 className="text-4xl font-bold text-center mb-4">Register</h2>
                     <form>
                         <div className="mb-4">
                             <label className="block text-gray-700">Username</label>
@@ -35,7 +40,7 @@ const Registration = () => {
                             <input type="text" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-400" />
                         </div>
 
-                        <button className="w-full bg-[#FFF346] text-black py-2 rounded-lg hover:bg-yellow-300 transition duration-300">Apply Now</button>
+                        <button className="w-full bg-[#FFF346] text-black font-bold py-2 rounded-lg hover:bg-yellow-300 transition duration-300">Apply</button>
                     </form>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,33 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '128': '32rem',
+      },
+      fontFamily: {
+        body: ['Scope One']
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    function({ addUtilities }) {
+      const newUtilities = {
+        '.text-shadow' : {
+          textShadow: `
+            3px 3px 0 #fff,
+            -3px 3px 0 #fff,
+            3px -3px 0 #fff,
+            -3px -3px 0 #fff,
+            3px 0 0 #fff,
+            -3px 0 0 #fff,
+            0 3px 0 #fff,
+            0 -3px 0 #fff
+          `
+        },
+      }
+      addUtilities(newUtilities, ['responsive', 'hover'])
+    }
+  ],
 }
 
